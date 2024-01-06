cbad814cbc6b0c790b35d5611800dfe24615205e
Talha2000 <amjad_maqsood90@hotmail.com>
2024-01-06 17:33:26 -0500

    removing sessions in homepage

--- front-end/src/app/(main)/homepage/page.jsx
+++ front-end/src/app/(main)/homepage/page.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { authOptions } from "../../api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+// import { authOptions } from "../../api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { redirect } from "next/navigation";
 import UserMenu from '../../../components/UserMenu';
 import HomeSideNav from '../../../components/HomeSideNav';
 import SearchBar from '../../../components/SearchBar';
 import UserCard from '../../../components/UserCard';
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);    
-  if (!session) {
-      redirect('/login');
-  }
-  console.log(session.sessionId)
+  // const session = await getServerSession(authOptions);    
+  // if (!session) {
+  //     redirect('/login');
+  // }
+  // console.log(session.sessionId)
   // Array simulating data from the database
   const cardsData = [
     { title: "The Consumption of Cars during the years", description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.", isUploadCard: false },
