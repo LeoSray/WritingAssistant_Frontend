3cd24e0882a9016238912747f21505babcc5e22b
Talha2000 <amjad_maqsood90@hotmail.com>
2024-01-10 00:56:16 -0500

    committing to do deployment

--- src/components/DocumentTitle.jsx
+++ src/components/DocumentTitle.jsx
@@ -5,7 +5,7 @@ import '../app/globals.css';
 
 function DocumentTitle() {
 //   const [title, setTitle] = useState('');
-
+// Hello
   return (
     <div className="documentTitle hidden md:flex md:pl-4 lg:pl-4 xl:pl-24 text-white">
       <button type="button">
